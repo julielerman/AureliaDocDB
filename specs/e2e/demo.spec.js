c6d22f8c4d1e072419b74c6c890dd33a60a3fd55
julie <julielerman@gmail.com>
2016-02-21 00:15:16 -0500

    delte more unneeded files from skeleton

--- specs/e2e/demo.spec.js
+++
@@ -1,9 +0,0 @@
-describe('aurelia skelleton app', function() {
-  it('should load the page', function() {
-    browser.ignoreSynchronization = true;
-    browser.get('http://localhost:9000/app');
-
-    browser.sleep(3000);
-    expect(browser.getTitle()).toBe('Welcome | Aurelia');
-  });
-});
