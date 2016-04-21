6872241a521b730bf8b2a363359714a8f49b78ab
julie <julielerman@gmail.com>
2016-04-21 15:18:31 -0400

    replaced one of the callbacks with a promise (then)

--- models/docDbDao.js
+++ models/docDbDao.js
@@ -2,20 +2,20 @@ var DocumentDBClient = require('documentdb').DocumentClient;
 var docdbUtils = require('./docdbUtils');
 
 function docDbDao(documentDBClient, databaseId, collectionId) {
-  this.client = documentDBClient;
-  this.databaseId = databaseId;
-  this.collectionId = collectionId;
+    this.client = documentDBClient;
+    this.databaseId = databaseId;
+    this.collectionId = collectionId;
 
-  this.database = null;
-  this.collection = null;
-  
+    this.database = null;
+    this.collection = null;
+
 
 }
 
 module.exports = docDbDao;
 
 docDbDao.prototype = {
-    
+
     init: function (callback) {
         var self = this;
 
@@ -25,14 +25,22 @@ docDbDao.prototype = {
 
             } else {
                 self.database = db;
-                docdbUtils.getOrCreateCollection(self.client, self.database._self, self.collectionId, function (err, coll) {
-                    if (err) {
-                        callback(err);
-
-                    } else {
+                docdbUtils.getOrCreateCollection(self.client, sel.database._self, self.collectionId)
+                    .then(function (coll) {
                         self.collection = coll;
-                    }
-                });
+                    }, function (err) {
+                        return err;
+                    });
+                // docdbUtils.getOrCreateCollection(self.client, self.database._self, self.collectionId, function (err, coll) {
+                //     if (err) {
+                //         callback(err);
+
+                //     } else {
+                //         self.collection = coll;
+                //     }
+                // })
+
+                ;
             }
         });
     },
@@ -74,11 +82,11 @@ docDbDao.prototype = {
                 callback(err);
 
             } else {
-                doc.Clan=item.Clan;
-                doc.Name=item.Name;
-                doc.ServedInOniwaban=item.ServedInOniwaban;
-                doc.DateOfBirth	=item.DateOfBirth;
-                doc.DateModified=Date.now();
+                doc.Clan = item.Clan;
+                doc.Name = item.Name;
+                doc.ServedInOniwaban = item.ServedInOniwaban;
+                doc.DateOfBirth = item.DateOfBirth;
+                doc.DateModified = Date.now();
                 self.client.replaceDocument(doc._self, doc, function (err, replaced) {
                     if (err) {
                         callback(err);
