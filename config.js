f49432ef8c3969dcbc00d420db115855dbafa9b5
julie <julielerman@gmail.com>
2016-04-23 13:57:33 -0400

    warning that the documentdb auth keys shown in config.js are getting replaced so don't even bother trying it ;)

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 var config = {}
 
 config.host = process.env.HOST || "https://msdndatapoints.documents.azure.com:443/";
+//I will be changing my personal auth keys after pushing this to git. sorry
 config.authKey = process.env.AUTH_KEY || "XN288CuHCsGn/wghvQMSUdSGGg2bu9vYhqJOopOnwa9Fj+fqSmofSzt7xR8C35jEeeEe3HaGCvSNLdo24e5BjQ==";
 config.databaseId = "Ninjas";
 config.collectionId = "Ninjas";
